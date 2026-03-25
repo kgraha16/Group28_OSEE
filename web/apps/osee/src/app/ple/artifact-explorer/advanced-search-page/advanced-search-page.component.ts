7c295e85ebe09734a69693af8c9be4e2b6f0c07e
Kris Graham <kgraha16@asu.edu>
2026-03-25 13:53:21 -0600

    Task 240 Implemented filteredAttributeSearch to search through the sorted columns using the signal from attributeSearch

--- web/apps/osee/src/app/ple/artifact-explorer/advanced-search-page/advanced-search-page.component.ts
+++ web/apps/osee/src/app/ple/artifact-explorer/advanced-search-page/advanced-search-page.component.ts
@@ -1593,6 +1593,11 @@ export class AdvancedSearchPageComponent implements OnInit {
 	});
 	
 	filteredAttributeColumns = computed<ColumnConfig[]>(() => {
+		const search = this.attributeSearch().toLowerCase().trim()
+		
+		return this.sortedAttributeColumns().filter(col =>
+			!search || col.label.toLowerCase().startsWith(search)
+		);
 	});
 
 	artifactTypes = toSignal(this.artifactService.allArtifactTypes);
