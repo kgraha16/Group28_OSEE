035cdb9b175ceecdd278d4de5d9e30aba03a6e76
Kris Graham <kgraha16@asu.edu>
2026-04-27 18:45:04 -0600

    Updated navigation playwright test and created column menu testing.

--- web/apps/osee/playwright/specs/asu/tests/ASP-navigation.e2e-spec.ts
+++ web/apps/osee/playwright/specs/asu/tests/ASP-navigation.e2e-spec.ts
@@ -5,4 +5,7 @@ test('test', async ({ page }) => {
   await page.getByRole('button').click();
   await page.getByText('public Product Line Engineering chevron_right').click();
   await page.getByRole('link', { name: 'Advanced Artifact Search' }).click();
+  
+  await expect(page).toHaveURL('http://localhost:4200/ple/artifact/explorer/search');
+  await expect(page.getByPlaceholder('Search')).toBeVisible();
 });
